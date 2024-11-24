81fcc614d1111ed134dba7f5e60a5287ad0173a1
puspasri <puspa09111984@gmail.com>
2024-11-24 21:19:44 +0530

    Add files via upload

---
+++ posnegchecking.py
@@ -0,0 +1,8 @@
+n=int(input("enter the age :"))
+if n>=18:
+    if n ==18:
+        print("age of the person ")
+    else:
+        print("he/she is adolescence")
+else:
+    print("he/she is adult")
